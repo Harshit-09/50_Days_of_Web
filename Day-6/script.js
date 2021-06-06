0d081a96998dbeba12acab7f3d00c465ac773889
Harshit <43957220+Harshit-09@users.noreply.github.com>
2021-06-06 17:56:53 +0530

    Update script.js

--- Day-6/script.js
+++ Day-6/script.js
@@ -9,7 +9,7 @@ function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4
 
     boxes.forEach(box => {
-        const boxTop = box.getBoundingClientReact.top
+        const boxTop = box.getBoundingClientRect.top
 
         if(boxTop < triggerBottom) {
             box.classList.add('show')
