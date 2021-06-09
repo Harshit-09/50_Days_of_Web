602a1fb10d90572b6ce857acb0151913a86c1589
Harshit <43957220+Harshit-09@users.noreply.github.com>
2021-06-09 15:59:44 +0530

    Update script.js

--- Day-9/script.js
+++ Day-9/script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'Yay']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
